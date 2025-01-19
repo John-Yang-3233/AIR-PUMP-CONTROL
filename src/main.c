ad0f7a544e5f241e04700b1572a007f9f328e10e
John-Yang-3233 <f94101020@gs.ncku.edu.tw>
2025-01-20 02:22:16 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,17 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 
+#define AIN2_PIN 10
+#define AIN1_PIN 12
+#define STBY_PIN 14
+#define BIN1_PIN 16
+#define BIN2_PIN 18
+
 #define SLEEP_TIME_MS   100
 #define PWM_frequency   100000
-#define PWM_PERIOD_NS   20000000
-#define POWER_PERCENTAGY   50
-#define PWM_DUTY_CYCLE    18000000
+#define PWM_PERIOD_NS   10000000
+#define PWM_DUTY_CYCLE   90
+#define PWM_PULSE    9000000
 
 #define PWM_MOTOR_A    DT_ALIAS(motor0)
 static const struct pwm_dt_spec motorA = PWM_DT_SPEC_GET(PWM_MOTOR_A); //pin 11 pwmA
@@ -22,22 +28,28 @@ static const struct device *gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW3_NODE, gpios);
 static struct gpio_callback button_cb_data;
 
-static volatile bool phase = 0;
+int phase = 0;
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-    phase = !phase; // Toggle phase
-
-    if(phase){
-        pwm_set_dt(&motorA, PWM_PERIOD_NS, PWM_DUTY_CYCLE);
+    phase += 1 ;
+    if(phase>2){
+        phase = 0 ;
+    }
+    if(phase = 1){
+        pwm_set_dt(&motorA, PWM_PERIOD_NS, PWM_PLUSE);
 		pwm_set_dt(&motorB, PWM_PERIOD_NS, 0);
         printk("motorA on\n");
 
-    } else {
-        pwm_set_dt(&motorB, PWM_PERIOD_NS, PWM_DUTY_CYCLE);
+    } else if(phase = 2){
+        pwm_set_dt(&motorB, PWM_PERIOD_NS, PWM_PLUSE);
         pwm_set_dt(&motorA, PWM_PERIOD_NS, 0);
         printk("motorB on\n");
-    }
+    } else if (phase = 0){
+        pwm_set_dt(&motorA, PWM_PERIOD_NS, 0);
+		pwm_set_dt(&motorB, PWM_PERIOD_NS, 0);
+        printk("all motor off\n");
+    } 
 
 }
 
@@ -52,16 +64,16 @@ int main(void)
         printk("Error: Button device %s is not ready\n", button.port->name);
         return -1;
     }
-	int ret = gpio_pin_configure(gpio_dev, 10, GPIO_OUTPUT );	//AIN2
-	ret = gpio_pin_configure(gpio_dev, 12, GPIO_OUTPUT );//AIN1
-	ret = gpio_pin_configure(gpio_dev, 14, GPIO_OUTPUT );//STBY
-	ret = gpio_pin_configure(gpio_dev, 16, GPIO_OUTPUT );//BIN1
-	ret = gpio_pin_configure(gpio_dev, 18, GPIO_OUTPUT );//BIN2
-	ret = gpio_pin_set(gpio_dev,10,1);
-	ret = gpio_pin_set(gpio_dev,12,0);
-	ret = gpio_pin_set(gpio_dev,14,1);
-	ret = gpio_pin_set(gpio_dev,16,1);
-	ret = gpio_pin_set(gpio_dev,18,0);
+	int ret = gpio_pin_configure(gpio_dev, AIN2_PIN, GPIO_OUTPUT );	//AIN2
+	ret = gpio_pin_configure(gpio_dev, AIN1_PIN, GPIO_OUTPUT );//AIN1
+	ret = gpio_pin_configure(gpio_dev, STBY_PIN, GPIO_OUTPUT );//STBY
+	ret = gpio_pin_configure(gpio_dev, BIN1_PIN, GPIO_OUTPUT );//BIN1
+	ret = gpio_pin_configure(gpio_dev, BIN2_PIN, GPIO_OUTPUT );//BIN2
+	ret = gpio_pin_set(gpio_dev,AIN2_PIN,1);
+	ret = gpio_pin_set(gpio_dev,AIN1_PIN,0);
+	ret = gpio_pin_set(gpio_dev,STBY_PIN,1);
+	ret = gpio_pin_set(gpio_dev,BIN1_PIN,1);
+	ret = gpio_pin_set(gpio_dev,BIN2_PIN,0);
 
     ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 
